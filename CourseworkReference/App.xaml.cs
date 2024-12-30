55cc2b7736355abb1973d106515413e0c052d045
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2024-12-30 19:52:59 +0545

    first commit

---
+++ CourseworkReference/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseworkReference
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ CourseworkReference/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseworkReference
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
